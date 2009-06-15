5226748bfc8b12873b20e0d51ea3b5d82e8f5c2d
Michael Bridgen <mikeb@lshift.net>
2009-06-15 02:59:53 +0100

    Debug and source, to help with debugging

--- install-libs.sh
+++ install-libs.sh
@@ -2,3 +2,4 @@
 mvn install:install-file -DgroupId=smack -DartifactId=smack -Dversion=3.1.0 -Dpackaging=jar -Dfile=smack.jar
 mvn install:install-file -DgroupId=smack -DartifactId=smackx -Dversion=3.1.0 -Dpackaging=jar -Dfile=smackx.jar
 mvn install:install-file -DgroupId=smack -DartifactId=smack-test -Dversion=3.1.0 -Dpackaging=jar -Dfile=smack-test.jar
+mvn install:install-file -DgroupId=smack -DartifactId=smack-debug -Dversion=3.1.0 -Dpackaging=jar -Dfile=smackx-debug.jar
