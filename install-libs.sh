e350ab8572323d536d08877a5d2e1e5c30132a53
sfussenegger <stf@molindo.at>
2009-09-04 22:46:06 +0800

    - changed all groupIds from smack to jivesoftware - changed installed version of smack jar to 3.1.0-pubsub1 (that should make it very easy to use patched version where version from central repo isn't working: with ejabberd)
    Signed-off-by: Michael Bridgen <mikeb@squaremobius.net>

--- install-libs.sh
+++ install-libs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-mvn install:install-file -DgroupId=smack -DartifactId=smack -Dversion=3.1.0 -Dpackaging=jar -Dfile=smack.jar
-mvn install:install-file -DgroupId=smack -DartifactId=smackx -Dversion=3.1.0 -Dpackaging=jar -Dfile=smackx.jar
-mvn install:install-file -DgroupId=smack -DartifactId=smack-test -Dversion=3.1.0 -Dpackaging=jar -Dfile=smack-test.jar
-mvn install:install-file -DgroupId=smack -DartifactId=smack-debug -Dversion=3.1.0 -Dpackaging=jar -Dfile=smackx-debug.jar
+mvn install:install-file -DgroupId=jivesoftware -DartifactId=smack -Dversion=3.1.0-pubsub1 -Dpackaging=jar -Dfile=smack.jar
+mvn install:install-file -DgroupId=jivesoftware -DartifactId=smackx -Dversion=3.1.0 -Dpackaging=jar -Dfile=smackx.jar
+mvn install:install-file -DgroupId=jivesoftware -DartifactId=smack-test -Dversion=3.1.0 -Dpackaging=jar -Dfile=smack-test.jar
+mvn install:install-file -DgroupId=jivesoftware -DartifactId=smack-debug -Dversion=3.1.0 -Dpackaging=jar -Dfile=smackx-debug.jar
